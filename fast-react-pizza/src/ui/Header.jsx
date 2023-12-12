4c52d8d9d80345ef13f8f7767fe5063c7c9dcb42
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-12 09:52:39 +0300

    Working with Color

--- fast-react-pizza/src/ui/Header.jsx
+++ fast-react-pizza/src/ui/Header.jsx
@@ -1,8 +1,8 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/searchOrder";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/searchOrder';
 export default function Header() {
   return (
-    <header>
+    <header className="bg-yellow-500">
       <Link to="/">Fast React Pizza Co.</Link>
       <SearchOrder />
       <p>Jibril A.</p>
