f226c8183f898f8e5bd8cac9642f439e97dfdbe2
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-12 10:11:21 +0300

    Styling Text

--- fast-react-pizza/src/ui/Header.jsx
+++ fast-react-pizza/src/ui/Header.jsx
@@ -1,11 +1,14 @@
 import { Link } from 'react-router-dom';
 import SearchOrder from '../features/order/searchOrder';
+import Username from '../features/users/Username';
 export default function Header() {
   return (
-    <header className="bg-yellow-500">
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-[5px]">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
-      <p>Jibril A.</p>
+      <Username />{' '}
     </header>
   );
 }
