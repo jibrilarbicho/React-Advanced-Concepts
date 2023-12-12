b10edbb203f0371a217f7e7f4c547e16a901445b
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-13 00:25:04 +0300

    Configuring Tailwind Custom Font Family

--- fast-react-pizza/src/ui/Header.jsx
+++ fast-react-pizza/src/ui/Header.jsx
@@ -3,7 +3,7 @@ import SearchOrder from '../features/order/searchOrder';
 import Username from '../features/users/Username';
 export default function Header() {
   return (
-    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6 flex justify-between items-center">
+    <header className="border-b  border-stone-200 bg-yellow-500 px-4 py-3 uppercase sm:px-6 flex justify-between items-center font-pizza">
       <Link to="/" className="tracking-[5px]">
         Fast React Pizza Co.
       </Link>
