f5a234074ca877a6fa368bab210f3b40f21e8824
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-13 09:50:26 +0300

    Styling th e Order Form

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -46,31 +46,32 @@ function CreateOrder() {
   const cart = fakeCart;
   const formErrors = useActionData(); //to et the action when the form is not submitted
   return (
-    <div>
-      <h2>Ready to order? Let's go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 tex-xl font-semibold">Ready to order? Let's go!</h2>
 
       <Form method="POST">
-        <div>
-          <label>First Name</label>
-          <input type="text" name="customer" required className="input" />
+        <div className="flex mb-5 flex-col sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <input type="text" name="customer" required className="input grow" />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input type="tel" name="phone" required className="input" />
-          </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
-        </div>
-
-        <div>
-          <label>Address</label>
-          <div>
-            <input type="text" name="address" required className="input" />
+        <div className="flex mb-5 flex-col sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className=" grow">
+            <input type="tel" name="phone" required className="input w-full" />
+         
+          {formErrors?.phone && <p className="text-xs mt-2 text-red-700 rounded-md  p-2 bg-red-100">{formErrors.phone}</p>}
           </div>
         </div>
 
-        <div>
+        <div className="flex mb-5 flex-col sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input type="text" name="address" required className="input w-full" />
+          </div>
+        </div>
+
+        <div className="mb-6 flex  items-center gap-6">
           <input
             type="checkbox"
             name="priority"
@@ -79,7 +80,7 @@ function CreateOrder() {
             // onChange={(e) => setWithPriority(e.target.checked)}
             className="h-6 w-6 accent-yellow-600 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor="priority" className="font-medium">Want to yo give your order priority?</label>
         </div>
         <input type="hidden" name="cart" value={JSON.stringify(cart)} />
         <div>
--- fast-react-pizza/src/ui/Button.jsx
+++ fast-react-pizza/src/ui/Button.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 
 export default function Button({children,disabled,to,type}) {
-    const base="bg-yellow-400 font-semibold uppercase text-stone-800  inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed "
+    const base="text-sm bg-yellow-400 font-semibold uppercase text-stone-800  inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed "
     const styles={
         primary:base+' px-4 py-3 md:py-4 md:px-6',
         small:base+' py-2 md:px-5 md:py2.5 text-xs',
