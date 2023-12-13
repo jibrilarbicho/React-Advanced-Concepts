b02deab636c19ad66376d86136a97385ff44d041
Jibril Arbicho <jibrilarbicho2@gmail.com>
2023-12-13 09:20:58 +0300

    Styling th e Cart

--- fast-react-pizza/src/ui/Button.jsx
+++ fast-react-pizza/src/ui/Button.jsx
@@ -4,7 +4,8 @@ export default function Button({children,disabled,to,type}) {
     const base="bg-yellow-400 font-semibold uppercase text-stone-800  inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed "
     const styles={
         primary:base+' px-4 py-3 md:py-4 md:px-6',
-        small:base+' py-2 md:px-5 md:py2.5 text-xs'
+        small:base+' py-2 md:px-5 md:py2.5 text-xs',
+        secondary:" px-4 py-3 md:py-4 md:px-6 border-2 border-stone-300 font-semibold uppercase text-stone-300  inline-block tracking-wide rounded-full hover:bg-stone-300 hover:text-stone-700 focus:bg-stone-800 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed "
     }
     if(to) return <Link className={styles[type]}>{children}</Link>
   return (
