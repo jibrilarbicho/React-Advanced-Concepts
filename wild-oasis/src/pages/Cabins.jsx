1a3c633af192be82bae72a2d0f58ecbeee984106
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-07-06 10:53:34 +0300

    React Hook-Form

--- wild-oasis/src/pages/Cabins.jsx
+++ wild-oasis/src/pages/Cabins.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import CabinTable from "../features/cabins/CabinTable";
+import Button from "../ui/Button";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 function Cabins() {
+  const [ShowForm, setShowForm] = useState(false);
   // useEffect(function () {
   //   getCabins().then((data) => console.log("data", data));
   // }, []);
@@ -13,6 +17,10 @@ function Cabins() {
       </Row>
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new Cabin
+        </Button>
+        {ShowForm && <CreateCabinForm />}
       </Row>
     </>
   );
