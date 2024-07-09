7a502e16702fdb7efb97dd31b5180a1f087ff343
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-07-09 23:19:36 +0300

    image

--- wild-oasis/src/services/apiCabins.js
+++ wild-oasis/src/services/apiCabins.js
@@ -20,7 +20,7 @@ export async function deleteCabin(id) {
   return data;
 }
 export async function createCabin(newCabin) {
-  const imageName = newCabin?.image?.name; // Assuming `newCabin` contains `imageName`
+  const imageName = newCabin?.image.name; // Assuming `newCabin` contains `imageName`
   const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
 
   // 1. Create cabin
@@ -36,12 +36,10 @@ export async function createCabin(newCabin) {
     throw new Error("Cabin could not be created");
   }
 
-  // 2. Upload image
   const { error: storageError } = await supabase.storage
     .from("cabin-images")
     .upload(imageName, newCabin.image);
 
-  // 3. Delete the cabin IF there was an error uploading the image
   if (storageError) {
     await supabase.from("cabins").delete().eq("id", data[0].id);
     console.error("Error uploading cabin image:", storageError);
