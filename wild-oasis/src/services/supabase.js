b62e2ad632393051bb9f6aa93ad3f72279d40c46
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-07-15 15:29:22 +0300

    .env Upadted

--- wild-oasis/src/services/supabase.js
+++ wild-oasis/src/services/supabase.js
@@ -2,7 +2,6 @@
 // dotenv.config({ path: "./.env" });
 
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://mcbqvejyalpckvwqljrv.supabase.co";
 
 const supabase = createClient(process.supabaseUrl, process.supabaseKey);
 export default supabase;
