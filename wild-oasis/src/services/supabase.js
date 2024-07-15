db3e4993590888341b208b43318102a57008b567
Jibril Arbicho <jibrilarbicho2@gmail.com>
2024-07-15 15:29:01 +0300

    .env Upadted

--- wild-oasis/src/services/supabase.js
+++ wild-oasis/src/services/supabase.js
@@ -3,7 +3,6 @@
 
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://mcbqvejyalpckvwqljrv.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jYnF2ZWp5YWxwY2t2d3FsanJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc0MDI3MDMsImV4cCI6MjAzMjk3ODcwM30.4E6BZqD70qT3aTV3mMsMSXjFu_I2HdGOaBYibvC0-Nk";
-const supabase = createClient(supabaseUrl, supabaseKey);
+
+const supabase = createClient(process.supabaseUrl, process.supabaseKey);
 export default supabase;
